6504bedb8a4e25ea2bfd1f60c6b0e1f3254f6e4e
Your Name <you@example.com>
2022-07-11 14:11:28 +0200

    Correction bug sur affichage comptes etc.

--- source/types.tsx
+++ source/types.tsx
@@ -9,3 +9,12 @@ export type PasswordMenuStackRouteParams = {
     MyPasswords: undefined
     AddPassword: undefined
 }
+
+export type fireStoreMainCollectionType = {
+    Login: string
+    Name: string
+    Password: string
+    Type: string
+    docId?: number
+    createdAt: Date
+  }
